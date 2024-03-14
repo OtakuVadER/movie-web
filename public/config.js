ed5794b75eb2585785014d473a241d8000e828bd
OtakuVadER <88473676+OtakuVadER@users.noreply.github.com>
2024-03-14 18:45:21 +0530

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,10 +1,9 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://movieweb-proxy.netlify.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzhjNjQ5MDVlZGRhZDY0OTBhZTc3MjBkMmZkZGUyMyIsInN1YiI6IjY1OWU1MjUzOTU5MGUzMDI1ZWUyMzg0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Hm9k0_5fe7z1g-vghfLUjenjYIC5vnNu0UZx1Qmnh54",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -12,9 +11,9 @@ window.__CONFIG__ = {
   // Whether to disable hash-based routing, leave this as false if you don't know what this is
   VITE_NORMAL_ROUTER: false,
 
-  // The backend URL to communicate with
-  VITE_BACKEND_URL: null,
+  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
+  VITE_BACKEND_URL: "https://mw-backend.lonelil.com",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
-  VITE_DISALLOWED_IDS: "",
+  VITE_DISALLOWED_IDS: ""
 };
