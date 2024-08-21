ff7c4d328d694ee51758bf2d4b8cc2f373418016
OtakuVadER <88473676+OtakuVadER@users.noreply.github.com>
2024-08-21 12:20:48 +0530

    make it work!?

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "https://movieweb-proxy.netlify.app",
+  VITE_CORS_PROXY_URL: "https://prvt-proxy.netlify.app/",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzhjNjQ5MDVlZGRhZDY0OTBhZTc3MjBkMmZkZGUyMyIsInN1YiI6IjY1OWU1MjUzOTU5MGUzMDI1ZWUyMzg0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Hm9k0_5fe7z1g-vghfLUjenjYIC5vnNu0UZx1Qmnh54",
@@ -12,7 +12,7 @@ window.__CONFIG__ = {
   VITE_NORMAL_ROUTER: false,
 
   // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
-  VITE_BACKEND_URL: "https://mw-backend.lonelil.ru",
+  VITE_BACKEND_URL: "",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: ""
